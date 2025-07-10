d9095fc485fa3db369f7ad08b4e9d5b93f65f8f7
Bogdan <b.nilov@innopolis.university>
2025-07-10 18:55:37 +0300

    lab05 - Bogdan Nilov JWT and Authentification

--- labs/lab05/backend/security/password.go
+++ labs/lab05/backend/security/password.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	_ "regexp"
 
-	_ "golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // PasswordService handles password operations
@@ -15,7 +15,7 @@ type PasswordService struct{}
 func NewPasswordService() *PasswordService {
 	// TODO: Implement this function
 	// Return a new PasswordService instance
-	return nil
+	return &PasswordService{}
 }
 
 // TODO: Implement HashPassword method
@@ -27,7 +27,14 @@ func NewPasswordService() *PasswordService {
 func (p *PasswordService) HashPassword(password string) (string, error) {
 	// TODO: Implement password hashing
 	// Use golang.org/x/crypto/bcrypt.GenerateFromPassword
-	return "", errors.New("not implemented")
+	if err := ValidatePassword(password); err != nil {
+		return "", err
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
 }
 
 // TODO: Implement VerifyPassword method
@@ -40,7 +47,7 @@ func (p *PasswordService) VerifyPassword(password, hash string) bool {
 	// TODO: Implement password verification
 	// Use bcrypt.CompareHashAndPassword
 	// Return true only if passwords match exactly
-	return false
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
 }
 
 // TODO: Implement ValidatePassword function
@@ -51,5 +58,31 @@ func (p *PasswordService) VerifyPassword(password, hash string) bool {
 func ValidatePassword(password string) error {
 	// TODO: Implement password validation
 	// Check length and basic complexity requirements
-	return errors.New("not implemented")
+	if len(password) < 6 {
+		return errors.New("password must be at least 6 characters long")
+	}
+	hasLetter := false
+	hasNumber := false
+	for _, r := range password {
+		if isLetter(r) {
+			hasLetter = true
+		}
+		if isNumber(r) {
+			hasNumber = true
+		}
+	}
+	if !hasLetter {
+		return errors.New("password must contain at least one letter")
+	}
+	if !hasNumber {
+		return errors.New("password must contain at least one number")
+	}
+	return nil
+}
+
+func isLetter(r rune) bool {
+	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z')
+}
+func isNumber(r rune) bool {
+	return r >= '0' && r <= '9'
 }
